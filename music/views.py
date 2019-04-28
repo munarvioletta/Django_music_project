326873a891db9c98e08468c13ee160d7d7a6b627
vme <munarvioletta@gmail.com>
2019-04-28 21:12:50 +0200

    adding title

--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 
@@ -15,11 +15,8 @@ from .models import Album
 def index(request):
     all_albums = Album.objects.all()
     template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-
-
-    }
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
     return HttpResponse(template.render(context, request))
 
 
