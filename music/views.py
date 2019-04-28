f1e0b127bb886fecaeeb3ad470d3b0015f7e2260
vme <munarvioletta@gmail.com>
2019-04-28 22:36:38 +0200

    show the details of the albums

--- music/views.py
+++ music/views.py
@@ -14,4 +14,4 @@ def detail(request, album_id):
         album = Album.objects.get(pk=album_id)
     except Album.DoesNotExist:
         raise Http404("Album does not exist")
-    return render(request, 'music/detail.html', {'albums': album})
+    return render(request, 'music/detail.html', {'album': album})
